b97b83c645023e1612a08836563898b94a07f321
student_2333 <lgc2333@126.com>
2023-02-04 17:30:00 +0800

    bump

--- nonebot_plugin_autoreply/__init__.py
+++ nonebot_plugin_autoreply/__init__.py
@@ -2,7 +2,7 @@ from nonebot.plugin import PluginMetadata
 
 from .__main__ import *
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __plugin_meta__ = PluginMetadata(
     "AutoReply",
     "这是一个一个一个自动回复插件啊啊啊",
