d1ec9351b3126c1556ef68b4fb360242caee53e4
student_2333 <lgc2333@126.com>
2023-02-19 01:27:14 +0800

    how foolish i am

--- nonebot_plugin_autoreply/__init__.py
+++ nonebot_plugin_autoreply/__init__.py
@@ -2,7 +2,7 @@ from nonebot.plugin import PluginMetadata
 
 from .__main__ import *  # noqa
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __plugin_meta__ = PluginMetadata(
     "AutoReply",
     "这是一个一个一个自动回复插件啊啊啊",
