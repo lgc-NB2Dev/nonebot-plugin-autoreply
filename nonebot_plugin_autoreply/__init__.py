f44d64f2a6f730d168fa5e9d3357613e820ed42f
student_2333 <lgc2333@126.com>
2023-06-05 01:05:25 +0800

    🎉 NoneBot 2.0 🚀

--- nonebot_plugin_autoreply/__init__.py
+++ nonebot_plugin_autoreply/__init__.py
@@ -4,10 +4,14 @@ from . import __main__ as __main__
 from .config import ConfigModel
 from .config import reload_replies as reload_replies
 
-__version__ = "0.2.10"
+__version__ = "0.2.11"
 __plugin_meta__ = PluginMetadata(
-    "AutoReply",
-    "配置文件高度可自定义的自动回复插件",
-    "这是一个一个一个自动回复插件啊啊啊",
-    ConfigModel,
+    name="AutoReply",
+    description="配置文件高度可自定义的自动回复插件",
+    usage="这是一个一个一个自动回复插件啊啊啊",
+    homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-autoreply",
+    type="application",
+    config=ConfigModel,
+    supported_adapters=["~onebot.v11"],
+    extra={"License": "MIT", "Author": "student_2333"},
 )
