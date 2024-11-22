5ca818b7c1a060dc439b2f004b65091e828ba5a2
烟雨あきな <69053823+YanyuAkina@users.noreply.github.com>
2024-11-22 13:10:00 +0800

    功能增强： 提供的路径为一个目录时可随机选择该目录下的图片或语音 (#23)
    * 功能增强： 提供的路径为一个目录时可随机选择该目录下的图片或语音
    
    * fix utf-8
    
    * 简化随机文件选择
    
    * refactor
    
    ---------
    
    Co-authored-by: student_2333 <lgc2333@126.com>

--- nonebot_plugin_autoreply/util.py
+++ nonebot_plugin_autoreply/util.py
@@ -1,6 +1,9 @@
-from typing import Any, Dict, Tuple, Union, cast
+import mimetypes
+import random
+from typing import Any, Dict, Optional, Tuple, Union, cast
 
 from anyio import Path
+from nonebot import logger
 from nonebot.adapters.onebot.utils import f2s
 from nonebot.adapters.onebot.v11 import (
     Bot,
@@ -63,6 +66,32 @@ async def get_var_dict(
     return normal_var, seg_var
 
 
+SEG_MIMETYPES = {
+    "image": "image/",
+    "record": "audio/",
+}
+
+
+async def process_res_seg(seg: MessageSegment):
+    file = seg.data.get("file")
+    if not (isinstance(file, str) and file.startswith("file:///")):
+        return
+
+    path = Path(file[8:])
+    if await path.is_dir():
+        f = await get_random_file(path, SEG_MIMETYPES[seg.type])
+        if not f:
+            logger.warning(f"No files matched expected file type for {seg.type}")
+            return
+        path = f
+
+    try:
+        seg.data["file"] = f2s(await path.read_bytes())
+    except Exception as e:
+        logger.error(f"Failed to read file {path}: {type(e).__name__}: {e}")
+        logger.opt(exception=e).debug("Stacktrace")
+
+
 async def replace_message_var(message: Message, var_dict: VarDictType) -> Message:
     normal_var, seg_var = var_dict
     message = cast(
@@ -77,8 +106,20 @@ async def replace_message_var(message: Message, var_dict: VarDictType) -> Messag
                     seg.data[k] = v.format(**normal_var)
 
         if seg.type in ("image", "record"):
-            file = seg.data.get("file")
-            if isinstance(file, str) and file.startswith("file:///"):
-                seg.data["file"] = f2s(await Path(file[8:]).read_bytes())
+            await process_res_seg(seg)
 
     return message
+
+
+async def get_random_file(base_path: Path, mime_pfx: str) -> Optional[Path]:
+    async def inner(p: Path):
+        async for x in p.iterdir():
+            if await x.is_dir():
+                async for y in inner(x):
+                    yield y
+
+            mime = mimetypes.guess_type(x.name)[0]
+            if mime and mime.startswith(mime_pfx):
+                yield x
+
+    return random.choice([x async for x in inner(base_path)])
